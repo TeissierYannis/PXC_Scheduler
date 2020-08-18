da345e1119b1848aad605f5caa4b4ffe81f78441
Teissier Yannis <yannis.teissier70@gmail.com>
2020-08-18 12:34:27 +0200

    NodeJS client fix - Working

--- public/NodeJS/Client/index.js
+++ public/NodeJS/Client/index.js
@@ -5,20 +5,20 @@ const fs = require('fs')
 let email, password
 
 let date
+
 process.argv.forEach(function (val, index, array) {
         if(index == 2) email = val
        
         if(index == 3) password = val
   });
-  
+
+console.log(email, password);
+
 (async () => {
     
-    date = new Date
-    
-    fs.appendFile('log-', 'Début de l\'update pour ' + email + "\n", function (err) {
-        if(err) throw err
-    })
-    
+     date = new Date
+
+
     if(email !== undefined && password !== undefined){
         function randomItems(max){
         
@@ -40,69 +40,57 @@ process.argv.forEach(function (val, index, array) {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
 
-        fs.appendFile('log-', 'Recherche de la page \n', function (err) {
-            if(err) throw err
-        })
-        
+
+
         await page.goto('https://www.planetminecraft.com/account/sign_in/', {waitUntil: 'networkidle2'});
-        
-        fs.appendFile('log-', 'Page trouvé \n', function (err) {
-            if(err) throw err
-        })
-        
-        fs.appendFile('log-', 'Ecriture de l\'email \n', function (err) {
-            if(err) throw err
-        })
-        
+
+        await page.screenshot({path: '1.png'})
+
+        await page.click('.qc-cmp-button');
+
+
         /** Email */
         await page.focus('#email')
         await page.keyboard.type(email)
-        
-        fs.appendFile('log-', 'Ecriture de l\'email réussie\n', function (err) {
-            if(err) throw err
-        })
-        
-        fs.appendFile('log-', 'Ecriture du mot de passe \n', function (err) {
-            if(err) throw err
-        })
-        
+
+        await page.screenshot({path: '2.png'})
+
+
         /** Password */
         await page.focus('#password')
         await page.keyboard.type(password)
-        
-        fs.appendFile('log-', 'Ecriture du mot de passe réussie\n', function (err) {
-            if(err) throw err
-        })
-        
-        fs.appendFile('log-', 'Tentative de connexion\n', function (err) {
-            if(err) throw err
-        })
-        
+
+
+        await page.screenshot({path: '3.png'})
+
+
         /** Connexion */
         await page.click('[value="Log in"]')
         await delay(5000)
-        
-        fs.appendFile('log-', 'Tentative de connexion réussie\n', function (err) {
-            if(err) throw err
-        })
-        
+
+        await page.screenshot({path: 'log.png'})
+
+
         fs.appendFile('log-', 'Redirection vers les différents packs\n', function (err) {
             if(err) throw err
         })
-        
+
+        await page.screenshot({path: '5.png'})
+
         // Redirection vers tous les packs
         await page.goto('https://www.planetminecraft.com/account/manage/all/', {waitUntil: 'networkidle2'})
         await delay(5000)
-        
+
         fs.appendFile('log-', 'Redirection vers les différents packs réussie \n', function (err) {
             if(err) throw err
         })
-        
-        
+
+        await page.screenshot({path: '6.png'})
+
         fs.appendFile('log-', 'Comptage des packs \n', function (err) {
             if(err) throw err
         })
-        
+
         // Nombre de pack sur le compte
         let packNumber = 0
         let exist = false
@@ -115,122 +103,72 @@ process.argv.forEach(function (val, index, array) {
             packNumber++
         }
         packNumber -= 1
-        
-        fs.appendFile('log-', packNumber + ' packs trouvé \n', function (err) {
-            if(err) throw err
-        })
-        
-        fs.appendFile('log-', 'Retour aux packs \n', function (err) {
-            if(err) throw err
-        })
-        
+
+        await page.screenshot({path: '7.png'})
+
         //Refresh
         await page.goto('https://www.planetminecraft.com/account/manage/all/', {waitUntil: 'networkidle2'})
         await delay(5000)
 
-        fs.appendFile('log-', 'Début des updates \n', function (err) {
-            if(err) throw err
-        })
-        
+        await page.screenshot({path: '8.png'})
+
         for(let i = 0; i < packNumber; i++){
             // Click sur la première submission
-            
-             fs.appendFile('log-', 'Premier pack trouvé \n', function (err) {
-                if(err) throw err
-            })
-            
+
             await page.click('[title="Edit Submission"]')
-            
-             fs.appendFile('log-', 'Edition du pack \n', function (err) {
-                if(err) throw err
-            })
-           
+
             await delay(8000)
-            
+
             await page.click('[class="material-icons md-24 assignment"]')
-            
-            fs.appendFile('log-', 'Onglet update trouvé \n', function (err) {
-                if(err) throw err
-            })
-    
+
+            await page.screenshot({path: '9.png'})
+
             await delay(6000)
 
-            
-             fs.appendFile('log-', 'Ecriture des maj \n', function (err) {
-                if(err) throw err
-            })
-            
-            // Ecriture dans update  
+            // Ecriture dans update
             let funct = randomItems(5)
             await page.evaluate((funct) => {
 
                 let itemsList = []
                 funct.forEach(e => itemsList += e + "<br>")
-                
+
                 document.querySelectorAll('iframe#content_ifr')[0].contentDocument.activeElement.innerHTML = itemsList
             }, funct)
 
-            
-             fs.appendFile('log-', 'Ecriture des updates réussie\n', function (err) {
-                if(err) throw err
-            })
-            
+            await page.screenshot({path: '10.png'})
+
             await delay(5000)
 
-             fs.appendFile('log-', 'Tentative de sauvegarde \n', function (err) {
-                if(err) throw err
-            })
-            
             // Sauvegarde des logs
             await page.click("#log_form > input.submit_log.site_btn")
             await delay(5000)
-            
-             fs.appendFile('log-', 'Sauvegarde maj réussie \n', function (err) {
-                if(err) throw err
-            })
-            
-             fs.appendFile('log-', 'Retour au pack \n', function (err) {
-                if(err) throw err
-            })
-            
+
+            await page.screenshot({path: '11.png'})
+
             await page.click("#ui-id-1 > span")
             await delay(5000)
-            
-             fs.appendFile('log-', 'Sauvegarde du pack \n', function (err) {
-                if(err) throw err
-            })
+
             // Sauvegarder
             await page.click("#tab_content > div.public_menu.embed_box > div > div.site_btn.save_submission")
             await delay(10000)
-            
-             fs.appendFile('log-', 'Sauvegarde réussie \n', function (err) {
-                if(err) throw err
-            })
-            
+
             await page.goto('https://www.planetminecraft.com/account/manage/all/', {waitUntil: 'networkidle2'})
             await delay(12000)
-            
-             fs.appendFile('log-', 'Premier pack trouvé \n', function (err) {
-                if(err) throw err
-            })
-            
+
             // Supprimer la precedente submission
             for(let j = 0; j <= i; j++){
 
                 await page.$eval('[title="Edit Submission"]', e => e.setAttribute("title", ""))
             }
         }
-        
-        fs.appendFile('log-', 'Fin de la mise à jour des packs \n', function (err) {
-                if(err) throw err
-            })
-        
+
+
         await browser.close();
     }
 
     function delay(time) {
-    return new Promise(function(resolve) { 
-        setTimeout(resolve, time)
-    });
+        return new Promise(function(resolve) {
+            setTimeout(resolve, time)
+        });
     }
 })();
